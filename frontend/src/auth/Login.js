fa9ac4289cc7f81b6c882c83aa8d3327b9d4f234
Sanjyoshree <jyotikasinha21@gmail.com>
2024-04-24 15:22:36 +0530

    Added validations to the pages

--- frontend/src/auth/Login.js
+++ frontend/src/auth/Login.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import login_img from ".././assets/images/login.png";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const newErrors = {};
+    if (!email.trim()) {
+      newErrors.email = "Please enter your email address";
+    }
+    if (!password.trim()) {
+      newErrors.password = "Please enter your password";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      // Add your login logic here
+      console.log("Email:", email);
+      console.log("Password:", password);
+    }
+  };
+
   return (
     <div className="wrapper">
       <div className="inner">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h3 style={{ marginBottom: "0px", textAlign: "left" }}>
             Welcome Back 🖐
           </h3>
@@ -22,7 +47,10 @@ const Login = () => {
               type="text"
               placeholder="Email Address"
               className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
+            {errors.email && <div className="error-message">{errors.email}</div>}
             <i className="zmdi zmdi-email"></i>
           </div>
 
@@ -31,21 +59,24 @@ const Login = () => {
               type="password"
               placeholder="Password"
               className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
+            {errors.password && <div className="error-message">{errors.password}</div>}
             <i className="zmdi zmdi-lock"></i>
           </div>
 
           <div className="remember-forgot">
             <div className="remember-me">
               <input type="checkbox" id="remember" />
-              <label for="remember"> Remember me</label>
+              <label htmlFor="remember"> Remember me</label>
             </div>
             <div className="forgot">
               <Link to="/reset">Forgot password?</Link>
             </div>
           </div>
 
-          <button>
+          <button type="submit">
             Sign in
             <i className="zmdi zmdi-arrow-right"></i>
           </button>
@@ -53,12 +84,13 @@ const Login = () => {
           <div id="hr">OR</div>
 
           <div id="login-btns">
-            <a href="">
+            <a href="g">
               <div
                 style={{ width: "32px", height: "32px", borderRadius: "50%" }}>
                 <img
                   className="google-icon"
                   src="https://i.ibb.co/ydLySMx/google.png"
+                  alt="Google"
                   width={"100%"}
                   height={"100%"}
                 />
@@ -67,13 +99,13 @@ const Login = () => {
 
             <br />
 
-            <a href="">
+            <a href="f">
               <div
                 className="logo"
                 style={{ width: "32px", height: "32px", borderRadius: "50%" }}>
                 <img
                   src="https://i.ibb.co/pnpDRC6/facebook.png"
-                  alt=""
+                  alt="Facebook"
                   width={"100%"}
                   height={"100%"}
                 />
@@ -107,3 +139,4 @@ const Login = () => {
 };
 
 export default Login;
+
--- frontend/src/auth/Register.js
+++ frontend/src/auth/Register.js
@@ -1,47 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import register_img from '.././assets/images/register-m2.png';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    gender: '',
+  });
+
+  const [validationMessages, setValidationMessages] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    gender: '',
+  });
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;      // This ids for Regex pattern for email validation
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+// we are using Regex pattern for password validation
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    // Clear validation message when user starts typing
+    setValidationMessages({ ...validationMessages, [e.target.name]: '' });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newValidationMessages = {};
+
+    // this is to Validate each field
+    if (!formData.firstName) {
+      newValidationMessages.firstName = '*';
+    }
+    if (!formData.lastName) {
+      newValidationMessages.lastName = '*';
+    }
+    if (!formData.email) {
+      newValidationMessages.email = 'Please enter your email address.';
+    } else if (!emailRegex.test(formData.email)) {
+      newValidationMessages.email = 'Please enter a valid email address.';
+    }
+    if (!formData.password) {
+      newValidationMessages.password = 'Please enter your password.';
+    } else if (!passwordRegex.test(formData.password)) {
+      newValidationMessages.password = 'Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, and one number.';
+    }
+    if (!formData.gender) {
+      newValidationMessages.gender = 'Please select your gender.';
+    }
+
+    // for Updation of validation messages
+    setValidationMessages(newValidationMessages);
+
+    // If there are validation errors, it will stop form submission
+    if (Object.keys(newValidationMessages).length !== 0) {
+      return;
+    }
+
+    // If all validations pass,  then it will proceed with form submission
+    console.log('Signing up with data:', formData);
+    // for Clear form data
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      gender: '',
+    });
+    // for Clear validation messages
+    setValidationMessages({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      gender: '',
+    });
+  };
+
   return (
     <div className="wrapper">
-      
       <div className="inner">
-        <form >
+        <form onSubmit={handleSubmit}>
           <h3>Register Now!</h3>
           <div className="form-group">
-            <input type="text" placeholder="First Name" className="form-control" />
-            <input type="text" placeholder="Last Name" className="form-control" />
+            <input type="text" placeholder="First Name" className="form-control" name="firstName" value={formData.firstName} onChange={handleChange} />
+            {validationMessages.firstName && <p className="validation-message">{validationMessages.firstName}</p>}
+            <input type="text" placeholder="Last Name" className="form-control" name="lastName" value={formData.lastName} onChange={handleChange} />
+            {validationMessages.lastName && <p className="validation-message">{validationMessages.lastName}</p>}
           </div>
           <div className="form-wrapper">
-            <input
-              type="text"
-              placeholder="Email Address"
-              className="form-control" />
+            <input type="text" placeholder="Email Address" className="form-control" name="email" value={formData.email} onChange={handleChange} />
+            {validationMessages.email && <p className="validation-message">{validationMessages.email}</p>}
             <i className="zmdi zmdi-email"></i>
           </div>
           <div className="form-wrapper">
-            <select name="" id="" className="form-control">
+            <select name="gender" className="form-control" value={formData.gender} onChange={handleChange}>
               <option value="" disabled selected>Gender</option>
               <option value="male">Male</option>
-              <option value="femal">Female</option>
+              <option value="female">Female</option>
               <option value="other">Other</option>
             </select>
+            {validationMessages.gender && <p className="validation-message">{validationMessages.gender}</p>}
             <i className="zmdi zmdi-caret-down" style={{fontSize: "17px"}}></i>
           </div>
           <div className="form-wrapper">
             <input type="number" placeholder="Age" className="form-control" />
           </div>
           <div className="form-wrapper">
-            <input
-              type="password"
-              placeholder="Password"
-              className="form-control" />
+            <input type="password" placeholder="Password" className="form-control" name="password" value={formData.password} onChange={handleChange} />
+            {validationMessages.password && <p className="validation-message">{validationMessages.password}</p>}
             <i className="zmdi zmdi-lock"></i>
           </div>
 
-          <button>
+          <button type="submit">
             Sign Up
             <i className="zmdi zmdi-arrow-right"></i>
           </button>
@@ -49,24 +126,22 @@ const Register = () => {
           <div id="hr">OR</div>
 
           <div id="login-btns">
-            <a href="">
-              <div
-                style={{ width: "32px", height: "32px", borderRadius: "50%" }}>
+            <a href="g">
+              <div style={{ width: "32px", height: "32px", borderRadius: "50%" }}>
                 <img
                   className="google-icon"
                   src="https://i.ibb.co/ydLySMx/google.png"
                   width={"100%"}
                   height={"100%"}
+                  alt="Google"
                 />
               </div>
             </a>
 
             <br />
 
-            <a href="">
-              <div
-                className="logo"
-                style={{ width: "32px", height: "32px", borderRadius: "50%" }}>
+            <a href="f">
+              <div className="logo" style={{ width: "32px", height: "32px", borderRadius: "50%" }}>
                 <img
                   src="https://i.ibb.co/pnpDRC6/facebook.png"
                   alt=""
@@ -78,25 +153,25 @@ const Register = () => {
           </div>
 
           <div id='sign-in'>
-              <span>
-                Already have an account?
-                <Link to="/"> Login</Link>
-              </span>
-            </div>
+            <span>
+              Already have an account?
+              <Link to="/"> Login</Link>
+            </span>
+          </div>
         </form>
         <div className="image-holder">
           <div id="mini-box">
-            <p style={{padding: "1vw"}}>
-Just one step<br />
-away from your<br />
-caretaker
+            <p style={{ padding: "1vw" }}>
+              Just one step<br />
+              away from your<br />
+              caretaker
             </p>
             <img src={register_img} alt="illustration" />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
