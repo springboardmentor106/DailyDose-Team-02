40943f213137148df2bcd43f88658b6406f7b817
sudhanshu--77 <stripathi1721@gmail.com>
2024-04-30 18:57:22 +0530

    code updated

--- Backend/config/emailConfig.js
+++ Backend/config/emailConfig.js
@@ -3,10 +3,10 @@ dotenv.config()
 import nodemailer from 'nodemailer'
 
 let transporter = nodemailer.createTransport({
-  // service: 'gmail',
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false, // true for 465, false for other ports
+  service: 'gmail',
+  // host: process.env.EMAIL_HOST,
+  // port: process.env.EMAIL_PORT,
+  // secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER, // Admin Email ID
     pass: process.env.EMAIL_PASS, // Admin Email Password
