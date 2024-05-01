651fd91258a40f44c0d8fdc3898bd9aceca0eebe
Mayank Singh <165413692+MayankGEHU@users.noreply.github.com>
2024-05-01 22:18:27 +0530

    Add files via upload

---
+++ Analytics.jsx
@@ -0,0 +1,94 @@
+import './Analytics.css';
+const Analytics = () => {
+    return (
+        <div className="dashboard-Analytics">
+       <div className="left-side-Analytics">
+        <div className="row-two-Analytics">
+          <div className="card-Analytics">
+          <div className="user-profile-card">
+            <div className="circle">
+
+            </div>
+            <div className="details">
+              <h3 id='Details-user'>Mrs.xyz</h3>
+              <h4 id='Details-user'>abc@gmail.com</h4>
+             <div className="Additional">
+             <div className="Additional-row-one">
+              <h3>49</h3>
+              <h3>B+</h3>
+             </div>
+             <div className="Additional-row-two">
+             <h4 id='Details-user'>Age</h4> 
+              <h4 id='Details-user'>Blood</h4>
+             </div>
+              </div>           
+            </div>
+    </div>
+          </div>
+          <div className="card-Analytics">
+          <div className="user-profile-card">
+            <div className="circle">
+
+            </div>
+            <div className="details">
+              <h3 id='Details-user'>Mrs.xyz</h3>
+              <h4 id='Details-user'>abc@gmail.com</h4>
+              <div className="Additional">
+             <div className="Additional-row-one">
+              <h3>49</h3>
+              <h3>B+</h3>
+             </div>
+             <div className="Additional-row-two">
+             <h4 id='Details-user'>Age</h4> 
+              <h4 id='Details-user'>Blood</h4>
+             </div>
+              </div>  
+            </div>
+    </div>
+          </div>
+          <div className="card-Analytics">
+          <div className="user-profile-card">
+            <div className="circle">
+          
+            </div>
+            <div className="details">
+              <h3 id='Details-user'>Mrs.xyz</h3>
+              <h4 id='Details-user'>abc@gmail.com</h4>
+              <div className="Additional">
+             <div className="Additional-row-one">
+              <h3>49</h3>
+              <h3>B+</h3>
+             </div>
+             <div className="Additional-row-two">
+             <h4 id='Details-user'>Age</h4> 
+              <h4 id='Details-user'>Blood</h4>
+             </div>
+              </div>  
+            </div>
+    </div>
+          </div>
+        </div>
+
+        <div className="row-one-Analytics">
+          <div className="row-one-card-one-Analytics">
+            
+          </div>
+        </div>
+        <div className="row-three-Analytics">
+        <div className="row-three-card-Analytics">
+          <h2>Progress</h2>
+        </div>
+        <div className="row-three-card-Analytics">
+          <h2>About User</h2>
+        </div>
+        </div>
+       </div>
+
+       <div className="right-side-Analytics">
+        
+       </div>
+  
+      </div>
+     );
+}
+export default Analytics;
