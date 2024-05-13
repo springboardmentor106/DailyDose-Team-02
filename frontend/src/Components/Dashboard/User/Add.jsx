223d1f057455e9644ea820e7b0468d969df8a496
Shreya <shreyadhadse77@gmail.com>
2024-05-13 22:45:16 +0530

    save button added in add jsx file

--- frontend/src/Components/Dashboard/User/Add.jsx
+++ frontend/src/Components/Dashboard/User/Add.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ReminderListAdd from '../User/ReminderList';
 import SideBar from "../User/SideBar"
 import './User.css'
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 const Add = () => {
   const reminders = [
     { Food: 'After Breakfast', activity: '1.0' },
@@ -17,12 +19,16 @@ const Add = () => {
       <SideBar/>
     <div className="add-page-container">
       <div className="dropdown">
+        
         <button className="dropdown-button" onClick={() => handleItemClick('goal')}>
-          <h4 id='heading'>Add Goal</h4>
+         <h4 id='heading'>Add Goals </h4>
         </button>
+       
         {selectedItem === 'goal' && (
+          
           <div className="dropdown-content">
             <div className="dropdown-container-left">
+              <h4>Add Goals</h4>
             <label>Title</label>
             <input type="text" />
             <label>Date</label>
@@ -37,7 +43,11 @@ const Add = () => {
             </div>
             </div>
             <div className="dropdown-container-right">
-            <label id='Label'>Decription</label>
+            <button className="btn" onClick={() => handleItemClick('goal')} style={{ padding: '6px 20px', fontSize: '10px', backgroundColor: '#E0DCF8', color: 'white' , marginLeft: '370px', borderRadius:'15px' ,}}>
+            <h4 id='heading' style={{ margin: 0, fontSize: '10px ',}}>Save </h4>
+            </button>
+              
+            <label id='Label'>Description</label>
             <textarea name="" id="right-input"></textarea>
             <label> End Date</label>
             <input type="date" id='End-date-input' />
@@ -52,27 +62,40 @@ const Add = () => {
       </div>
       <div className="dropdown">
         <button className="dropdown-button" onClick={() => handleItemClick('habit')}>
-          <h4 id='heading'> Add Remainders</h4>
+          <h4 id='heading'> Add Reminders</h4>
         </button>
         {selectedItem === 'habit' && (
             <div className="dropdown-content">
             <div className="dropdown-container-left">
+            <h4>Add Reminders</h4>
             <label>Title</label>
             <input type="text" />
             <label>Date</label>
             <input type="date" />
             <div className="catogry-feilds">
+
             <label id='Schedule-lable'>Schedule</label>
+
+            <div className="bordered-div" style={{ border: '1px solid black',borderRadius: '8px', padding: '5px' }}>
+
             <ReminderListAdd reminders={reminders} />
+            <button className="btn" onClick={() => handleItemClick('goal')} style={{ padding: '5px 8px', fontSize: '10px', backgroundColor: '#6A58DC', color: 'white' , marginLeft: '110px' }}>
+            <h4 id='heading' style={{ margin: 0, fontSize: '10px' }}>+ Add </h4>
+            </button>
+            </div>
+
             </div>
             </div>
             <div className="dropdown-container-right">
+            <button className="btn" onClick={() => handleItemClick('goal')} style={{ padding: '6px 20px', fontSize: '10px', backgroundColor: '#E0DCF8', color: 'white' , marginLeft: '380px', borderRadius:'15px' ,}}>
+            <h4 id='heading' style={{ margin: 0, fontSize: '10px ',}}>Save </h4>
+            </button>
             <label> Start Date</label>
             <input type="date" />
             <label> End Date</label>
             <input type="date" />
             <div className="catogry-feilds-right">
-              <label>Decription</label>
+              <label>Description</label>
             <textarea name="" id="right-input-two"></textarea>
             </div>
             </div>
@@ -85,7 +108,9 @@ const Add = () => {
         </button>
         {selectedItem === 'reminder' && (
             <div className="dropdown-content">
+             
             <div className="dropdown-container-left">
+            <h4 id='heading'> Add Habits</h4>
             <label>Title</label>
             <input type="text" />
             <label>Date</label>
@@ -100,7 +125,10 @@ const Add = () => {
             </div>
             </div>
             <div className="dropdown-container-right">
-            <label>Decription</label>
+            <button className="btn" onClick={() => handleItemClick('goal')} style={{ padding: '6px 20px', fontSize: '10px', backgroundColor: '#E0DCF8', color: 'white' , marginLeft: '380px', borderRadius:'15px' ,}}>
+            <h4 id='heading' style={{ margin: 0, fontSize: '10px ',}}>Save </h4>
+            </button>
+            <label>Description</label>
             <input type="text" id='right-input'/>
             <label> End Date</label>
             <input type="date" />
