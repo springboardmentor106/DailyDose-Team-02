df1a7a76d680b077ee7c43e93881a9f055c8e5ab
MayankGEHU <minku6195@gmail.com>
2024-05-14 17:24:11 +0530

    make minor changes in dashboard

--- frontend/src/Components/Dashboard/User/StaticDataUser.js
+++ frontend/src/Components/Dashboard/User/StaticDataUser.js
@@ -4,7 +4,6 @@ const profileinfo = [
        age: 30, 
        location: "Delhi, India" }
   ];
-  
   /* user - reminder list static data for userdashboard */
   const reminders = [
     { time: '10:05 PM', activity: 'Gardening' },
@@ -15,7 +14,6 @@ const profileinfo = [
     { time: '10:30 PM', activity: 'Watching a movie' },
     { time: '10:35 PM', activity: 'Preparing for bed' }
   ];
-
   /* Remider-list-data for Add */
   const remindersAddData = [
     { Food: 'After Breakfast', activity: '1.0' },
@@ -23,5 +21,11 @@ const profileinfo = [
     { Food: 'After Dinner', activity: '1.0' },
   ];
 
-  export { profileinfo, reminders , remindersAddData };
+  /*  User chart data */
+  const Month=[
+    {name:'Jan', performance:20},{name:'Feb', performance:50},{name:'Mar', performance:80},{name:'Apr', performance:60},
+    {name:'May', performance:30},{name:'Jun', performance:20},{name:'Jul', performance:40},{name:'Aug', performance:90},
+    {name:'Sep', performance:70},{name:'Oct', performance:60},{name:'Nov', performance:50},{name:'Dec', performance:20}
+]
+  export { profileinfo, reminders , remindersAddData,Month };
   
