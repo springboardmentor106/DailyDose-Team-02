45b869f009c17f8eac7420364fb749d108982c6f
naksh08 <nakshatradeep21@gmail.com>
2024-05-14 13:24:43 +0530

    Modified navbar and grid layout in userDashboard

--- frontend/src/Components/userDashboard/UserHome.js
+++ frontend/src/Components/userDashboard/UserHome.js
@@ -8,7 +8,11 @@ const Nav = () => {
       <div className="user__body">
         <UserNav />
         <div className="main__content">
-          
+          <div className="boxes"  id="welcome">Welcome back</div>
+          <div className="boxes"  id="profile">Profile </div>
+          <div className="boxes"  id="calender">calender</div>
+          <div className="boxes"  id="chart">chart</div>
+          <div className="boxes"  id="progress">progress</div>
         </div>
       </div>
     </>
