608d0f181728ac9d36a76bd32353e6a7c32bfc9b
naksh08 <nakshatradeep21@gmail.com>
2024-05-28 00:10:23 +0530

    modified user part

--- frontend/src/Components/Dashboard/dashComponents/Notification.js
+++ frontend/src/Components/Dashboard/dashComponents/Notification.js
@@ -3,61 +3,7 @@ import UserNav from '../../userDashboard/UserNav'
 import UserProfile from '../dashComponents/UserProfile'
 const Notification = () => {
   return (
-    // <div className="main1" style={{opacity:"1"}}>
-    //     <div className="nav-bar">
-    //       <UserNav/>
-    //     </div>
-    //     <UserProfile/>
-    //     <div className="notify" style={{paddingLeft:"110px",width:"100%"}}>
-    //     <div className='Notification-heading' 
-    //     style={{backgroundColor:"#6a58dc",color:"white", borderRadius:"20px", padding:"15px",
-    //       display:"flex", flexDirection:"row", justifyContent:"space-between",margin:"10px"
-    //     }}>
-    //         <div><h4><strong>Notification</strong></h4></div>
-    //         <div><button type="button" className="btn-close" ></button></div>
-
-    //     </div>
-    //     <div className="Nodtification-body">
-        
-
-    //       <div className="card" style={{borderRadius:"20px",marginBottom:"10px"}}>
-    //         <div className="card-title" style={{backgroundColor:"white", color:"black",marginLeft:"1%",display:"flex",justifyContent:"flex-start",gap:"10px"}}>
-    //          <div><h5><strong>Goal Achived </strong></h5></div>
-    //          <div><h5>12:00</h5> </div>
-    //         </div>
-    //         <div className="card-body" style={{display:"flex", justifyContent:"space-between", flexDirection:"row",width:"97%"}}>
-    //           <div><p>Congratulation !!! you have achive your goal "by reading Daily"</p></div>
-    //           <div><button className='btn btn-sm' style={{backgroundColor:"#6a58dc",color:"white"}}>Go to Goal</button></div>
-    //         </div>
-            
-    //       </div>
-    //       <div className="card" style={{borderRadius:"20px",marginBottom:"10px"}}>
-    //         <div className="card-title" style={{backgroundColor:"white", color:"black",marginLeft:"1%",display:"flex",justifyContent:"flex-start",gap:"10px"}}>
-    //          <h5><strong>Goal Achive</strong></h5>
-    //          <div><h5>12:00</h5> </div>
-    //         </div>
-    //         <div className="card-body" style={{display:"flex", justifyContent:"space-between", flexDirection:"row",width:"97%"}}>
-    //           <div><p>Congratulation !!! you have achive your goal "by reading Daily"</p></div>
-    //           <div><button className='btn btn-sm' style={{backgroundColor:"#6a58dc",color:"white"}}>Go to Goal</button></div>
-    //         </div>
-            
-    //       </div>
-
-    //       <div className="card" style={{borderRadius:"20px",marginBottom:"10px"}}>
-    //         <div className="card-title" style={{backgroundColor:"white", color:"black",marginLeft:"1%",display:"flex",justifyContent:"flex-start",gap:"10px"}}>
-    //          <h5><strong>Goal Achive</strong></h5>
-    //          <div><h5>12:00</h5> </div>
-    //         </div>
-    //         <div className="card-body" style={{display:"flex", justifyContent:"space-between", flexDirection:"row",width:"97%"}}>
-    //           <div><p>Congratulation !!! you have achive your goal "by reading Daily"</p></div>
-    //           <div><button className='btn btn-sm' style={{backgroundColor:"#6a58dc",color:"white"}}>Go to Goal</button></div>
-    //         </div>
-            
-    //       </div>
-
-    //    </div>
-    //     </div>
-    // </div>
+    
 
     <div className="modal fade" id="staticBackdrop3" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div className="modal-dialog model-lg modal-dialog-scrollable">
--- frontend/src/Components/Dashboard/dashComponents/UserProfile.js
+++ frontend/src/Components/Dashboard/dashComponents/UserProfile.js
@@ -1,140 +1,302 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
-import Notification from '../dashComponents/Notification'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Notification from "../dashComponents/Notification";
 const UserProfile = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleLogout = () => {
-    localStorage.clear()
-    navigate("/login")
-  }
+    localStorage.clear();
+    navigate("/login");
+  };
   return (
     <div>
-
-
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">      
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
         <div className="modal-dialog modal-sm">
-          <div className="modal-content" style={{backgroundColor:"#6a58dc",width:"fit-content",marginTop: "150%",marginLeft: "-130%"}}>
-            <div className="modal-body" style={{display:"flex", flexDirection:"column", justifyContent:"flex-evenly"}}>
-              
-              <div><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop1" style={{backgroundColor:"#e0dcf8",width:"100%"}}>Edit Profile</button></div>
-              <div><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" style={{backgroundColor:"#e0dcf8",width:"100%"}}>Setting</button></div>
-              <div><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop3" style={{backgroundColor:"#e0dcf8",width:"100%"}}>Notification</button></div>
-              <div><button type="button" class="btn" style={{backgroundColor:"#e0dcf8",width:"100%"}} onClick={() => handleLogout()}>Logout</button></div>
+          <div
+            className="modal-content"
+            style={{
+              backgroundColor: "#6a58dc",
+              width: "fit-content",
+              marginTop: "150%",
+              marginLeft: "-130%",
+            }}>
+            <div
+              className="modal-body"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-evenly",
+              }}>
+              <div>
+                <button
+                  type="button"
+                  class="btn"
+                  data-bs-toggle="modal"
+                  data-bs-target="#staticBackdrop1"
+                  style={{ backgroundColor: "#e0dcf8", width: "100%" }}>
+                  Edit Profile
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  class="btn"
+                  data-bs-toggle="modal"
+                  data-bs-target="#staticBackdrop2"
+                  style={{ backgroundColor: "#e0dcf8", width: "100%" }}>
+                  Setting
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  class="btn"
+                  data-bs-toggle="modal"
+                  data-bs-target="#staticBackdrop3"
+                  style={{ backgroundColor: "#e0dcf8", width: "100%" }}>
+                  Notification
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  class="btn"
+                  style={{ backgroundColor: "#e0dcf8", width: "100%" }}
+                  onClick={() => handleLogout()}>
+                  Logout
+                </button>
+              </div>
             </div>
           </div>
         </div>
-        </div>
+      </div>
       {/* ----Model----- */}
-      <div className="modal fade" id="staticBackdrop1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div
+        className="modal fade"
+        id="staticBackdrop1"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true">
         <div className="modal-dialog">
-        <div className="modal-content">
-      <div className="modal-header" 
-        style={{
-          backgroundImage:"linear-gradient(to right,#e7ccf9,#b7d7f3)",
-          height:"90px"
-        }}>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body" >
-        <div className="profile-header" style={{display:"flex", justifyContent:"flex-start"}}>
-            <div style={{
-                width:"150px", height:"150px", borderRadius:"100%", background:"#9186d9", marginTop:"-20%"
-            }}></div>
-            <div>
-                <strong>Username</strong> <p>username@gmail.com</p>
+          <div className="modal-content">
+            <div
+              className="modal-header"
+              style={{
+                backgroundImage: "linear-gradient(to right,#e7ccf9,#b7d7f3)",
+                height: "90px",
+              }}>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
             </div>
-        </div>
-        <div className="profile-body" style={{display:"flex", flexDirection:"row", justifyContent:"space-evenly"}}>
-            <div className="profile-left">
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Name</label>
-                    <input type="text" class="form-control" />
+            <div className="modal-body">
+              <div
+                className="profile-header"
+                style={{ display: "flex", justifyContent: "flex-start" }}>
+                <div
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    borderRadius: "100%",
+                    background: "#9186d9",
+                    marginTop: "-20%",
+                  }}></div>
+                <div>
+                  <strong>Username</strong> <p>username@gmail.com</p>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email</label>
+              </div>
+              <div
+                className="profile-body"
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                }}>
+                <div className="profile-left">
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Name
+                    </label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Email
+                    </label>
                     <input type="email" class="form-control" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Phone no</label>
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Phone no
+                    </label>
                     <input type="text" class="form-control" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Date of birth</label>
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Date of birth
+                    </label>
                     <input type="date" class="form-control" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Address</label>
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Address
+                    </label>
                     <input type="text" class="form-control" />
+                  </div>
                 </div>
+                <div className="profile-right">
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Disease
+                    </label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Allergy
+                    </label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Surgical History
+                    </label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Genetic Disorder
+                    </label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                      Discription
+                    </label>
+                    <input type="text" class="form-control" />
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="profile-right">
-            <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Disease</label>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Allergy</label>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Surgical History</label>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Genetic Disorder</label>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Discription</label>
-                    <input type="text" class="form-control" />
-                </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary btn-sm"
+                data-bs-dismiss="modal">
+                Cancel
+              </button>
+              <button type="button" className="btn btn-primary btn-sm">
+                Save
+              </button>
             </div>
+          </div>
         </div>
-        
       </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary btn-sm" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" className="btn btn-primary btn-sm">Save</button>
-      </div>
-    </div>
-  </div>
-</div>
 
-{/* setting model */}
-<div className="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      {/* setting model */}
+      <div
+        className="modal fade"
+        id="staticBackdrop2"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true">
         <div className="modal-dialog">
-        <div className="modal-content">
-        <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Setting</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <div className="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                Setting
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
+            </div>
+            <div
+              className="modal-body"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+              }}>
+              <div
+                class="form-check form-switch"
+                style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  <strong>Dark Mode</strong>
+                </div>
+                <div>
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                  />
+                </div>
+              </div>
+              <div
+                class="form-check form-switch"
+                style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  <strong>Notification</strong>
+                </div>
+                <div>
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                  />
+                </div>
+              </div>
+              <div
+                class="form-check form-switch"
+                style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  <strong>Google calender</strong>
+                </div>
+                <div>
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                  />
+                </div>
+              </div>
+              <div
+                class="form-check form-switch"
+                style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  <strong>Location</strong>
+                </div>
+                <div>
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="modal-body" style={{
-        display:"flex", flexDirection:"column", justifyContent:"flex-start"
-      }}>
-        <div class="form-check form-switch" style={{display:"flex", justifyContent:"space-between"}}>
-          <div><strong>Dark Mode</strong></div>
-          <div><input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/></div>
-        </div>
-        <div class="form-check form-switch" style={{display:"flex", justifyContent:"space-between"}}>
-          <div><strong>Notification</strong></div>
-          <div><input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/></div>
-        </div>
-        <div class="form-check form-switch" style={{display:"flex", justifyContent:"space-between"}}>
-          <div><strong>Google calender</strong></div>
-          <div><input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/></div>
-        </div>
-        <div class="form-check form-switch" style={{display:"flex", justifyContent:"space-between"}}>
-          <div><strong>Location</strong></div>
-          <div><input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/></div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
 
-      <Notification/>
+      <Notification />
     </div>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
